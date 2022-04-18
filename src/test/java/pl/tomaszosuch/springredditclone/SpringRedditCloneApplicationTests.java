c41632694f0b6b5a4ad24a4535e7aebde3a8cb8d
Tomasz Osuch <tomasz.osuch@gmail.com>
2022-04-18 10:18:10 +0200

    first commit

---
+++ src/test/java/pl/tomaszosuch/springredditclone/SpringRedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomaszosuch.springredditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
